f504277636f1cbd01c182ee473d4c7c3d2b99b3a
Michael Myers <michael.myers@wustl.edu>
2023-11-23 10:53:10 -0600

    Added colors.jl, layouts.jl

--- src/ObservableCortex.jl
+++ src/ObservableCortex.jl
@@ -1,5 +1,20 @@
+
 module ObservableCortex
 
-# Write your package code here.
+using CIFTI
+using CorticalSurfaces
+using CorticalParcels
+using Colors
+using Chain
+using Observables
+using GLMakie
+
+include("layouts.jl")
+export ViewDirection, Lateral, Medial, Dorsal, Ventral
+export OrthoGraphicView, OrthographicLayout, default_layout
+
+include("colors.jl")
+export coolhot, videen_style
 
 end
+
---
+++ src/layouts.jl
@@ -0,0 +1,42 @@
+
+@enum ViewDirection Lateral Medial Dorsal Ventral
+
+struct OrthographicView
+	hemisphere::BrainStructure
+	direction::ViewDirection
+end
+
+struct OrthographicLayout
+	views::Matrix{OrthographicView}
+end
+
+const default_layout = OrthographicLayout(
+	[
+		OrthographicView(L, Lateral) OrthographicView(R, Lateral);
+		OrthographicView(L, Medial)  OrthographicView(R, Medial)
+	]
+)
+
+# @match doesn't seem to be able to work with enum types within an OrthographicView,
+# unfortunately; but this workaround with casting to a tuple of Ints is not too bad
+function azimuth(v::OrthographicView)::Float64
+	@match (Int(v.hemisphere), Int(v.direction)) begin
+		(0, 0) =>  π   # L, Lateral
+		(0, 1) =>  0   # L, Medial
+		(1, 0) =>  0   # R, Lateral
+		(1, 1) =>  π   # R, Medial
+		(_, 2) => -π/2 # _, Dorsal
+		(_, 3) =>  π/2 # _, Ventral
+	end
+end
+
+function elevation(v::OrthographicView)::Float64
+	@match (Int(v.hemisphere), Int(v.direction)) begin
+		(_, 0) =>  0   # _, Lateral
+		(_, 1) =>  0   # _, Medial
+		(_, 2) =>  π/2 # _, Dorsal
+		(_, 3) => -π/2 # _, Ventral
+	end
+end
+
+
