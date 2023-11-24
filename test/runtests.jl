1b9160817d0a550de348be16bf91eb32fa77a4ce
Michael Myers <michael.myers@wustl.edu>
2023-11-24 10:44:06 -0600

    Deleted tests for now while I figure out how to get Makie to test OK on github

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,6 @@
 using ObservableCortex
 using Test
 using JLD
-using CairoMakie
 using CIFTI
 using CorticalSurfaces
 using Colors
@@ -27,23 +26,5 @@ c = CorticalSurface(hemL, hemR)
 		[(L, Lateral) (L, Medial) (L, Dorsal) (L, Ventral)]
 	)
 	@test size(custom_views) == (1, 4)
-
-	fig = Figure(; size = (800, 600))
-	montage = Montage(views = default_views, grid = fig.layout, surface = c)
-	colors = collect(1.0:size(c, Exclusive()))
-	mesh!(montage, colors; colormap = coolhot)
-
-	fig = Figure(; size = (800, 600))
-	montage = Montage(views = default_views, grid = fig.layout, surface = c)
-	colors = [RGB(α, α, α) for α in range(0, 1; length = size(c, Exclusive()))]
-	mesh!(montage, colors; colormap = coolhot)
-
-	fig = Figure(; size = (1200, 300))
-	montage = Montage(views = custom_views, grid = fig.layout, surface = c)
-	colors = [RGB(α, α, α) for α in range(0, 1; length = size(c, Exclusive()))]
-	mesh!(montage, colors; colormap = coolhot)
-	colgap!(montage.grid, 2, -100)
-	colgap!(montage.grid, 3, -220)
 end
-
 
