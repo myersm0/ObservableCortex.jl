bed1ce69b55834d864f807c12944c8b45b08d0ea
Michael Myers <michael.myers@wustl.edu>
2023-11-27 21:09:08 -0600

    Brought in axis/axes tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using JLD
 using CIFTI
 using CorticalSurfaces
 using Colors
+using GLMakie
 
 # first we need to setup the surface
 surface_dir = joinpath(dirname(@__FILE__), "..", "data")
@@ -29,5 +30,28 @@ c = CorticalSurface(hemL, hemR)
 		[(L, Lateral) (L, Medial) (L, Dorsal) (L, Ventral)]
 	)
 	@test size(custom_views) == (1, 4)
+
+	fig = Figure(; size = (800, 600))
+	montage = Montage(views = default_views, grid = fig.layout, surface = c)
+	m, n = size(default_views)
+	for i in 1:m
+		for j in 1:n
+			ax = montage.axes[i, j]
+			@test axis(montage, i, j) === ax
+			@test ax in axes(montage, i, :)
+			@test length(axes(montage, i, :)) == 2
+			@test ax in axes(montage, :, j)
+			@test length(axes(montage, :, j)) == 2
+
+			which_view = montage.views[i, j]
+			@test axis(montage, which_view) === ax
+			@test axis(montage, which_view.hemisphere, which_view.direction) === ax
+			@test axis(montage, (which_view.hemisphere, which_view.direction)) === ax
+
+			which_hem = montage.views[i, j].hemisphere
+			@test length(axes(montage, which_hem)) == 2
+			@test ax in axes(montage, which_hem)
+		end
+	end
 end
 
