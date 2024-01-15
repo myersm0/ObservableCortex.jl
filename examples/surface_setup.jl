bbcd8bdeeae5829c0b1606dd35af84df76aa5923
Michael Myers <michael.myers@wustl.edu>
2024-01-15 12:03:23 -0600

    Update demo, etc for new version of CorticalSurfaces

--- examples/surface_setup.jl
+++ examples/surface_setup.jl
@@ -7,12 +7,12 @@ temp = load(joinpath(surface_dir, "MSC01.jld"))
 surfL = temp["pointsets"]["midthickness"][L]
 mwL = temp["medial wall"][L]
 triangleL = temp["triangle"][L]
-hemL = Hemisphere(surfL, mwL; triangles = triangleL)
+hemL = Hemisphere(L, surfL, mwL; triangles = triangleL)
 
 surfR = temp["pointsets"]["midthickness"][R]
 mwR = temp["medial wall"][R]
 triangleR = temp["triangle"][R]
-hemR = Hemisphere(surfR, mwR; triangles = triangleR)
+hemR = Hemisphere(R, surfR, mwR; triangles = triangleR)
 
 c = CorticalSurface(hemL, hemR)
 
