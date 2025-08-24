4a8513d81b5ea2382299fb2dafadc047e56a35fc
Michael Myers <michael.myers@wustl.edu>
2025-08-24 14:06:59 -0500

    refactored plotting functions, to factor out computation of colors/values and their alignment to surface

--- src/plot.jl
+++ src/plot.jl
@@ -1,50 +1,15 @@
 
-function Makie.mesh!(
-		montage::Montage, values::Vector{T}; kwargs...
-	) where T <: Union{AbstractFloat, Colorant}
-	if length(values) != size(montage.surface)
-		length(values) == size(montage.surface, Exclusive()) || error(DimensionMismatch)
-		pad_with = T <: Colorant ? surf_color : NaN
-		values = pad(values, montage.surface; sentinel = pad_with)
+
+function Makie.mesh!(montage::Montage, values::AbstractVector; kwargs...)
+	values_to_plot = @chain begin
+		align_values(values, montage)
+		compute_colors(_; kwargs...)
 	end
 	for (i, ax) in enumerate(montage.axes)
 		which_hem = montage.views[i].hemisphere
 		verts = vertices(montage.surface[which_hem], Bilateral(), Inclusive())
 		mesh!(
-			ax, montage.meshes[which_hem]..., color = values[verts]; 
-			kwargs...
-		)
-	end
-end
-
-function Makie.mesh!(
-		montage::Montage, values::Vector{<:Integer}; pad_with = 0, kwargs...
-	)
-	if length(values) != size(montage.surface)
-		length(values) == size(montage.surface, Exclusive()) || error(DimensionMismatch)
-		values = pad(values, montage.surface; sentinel = pad_with)
-	end
-
-	colormap = haskey(kwargs, :colormap) ? 
-		kwargs[:colormap] : 
-		distinguishable_colors(min(64, length(unique(values))))
-
-	if typeof(colormap) <: AbstractVector
-		colorrange = haskey(kwargs, :colorrange) ?
-			kwargs[:colorrange] :
-			(minimum(values), maximum(values))
-		colors = colorize.(values; colormap = colormap, colorrange = colorrange)
-	else # AbstractDict case
-		colors = [colormap[v] for v in values]
-	end
-
-	colors[medial_wall(montage.surface)] .= surf_color
-
-	for (i, ax) in enumerate(montage.axes)
-		which_hem = montage.views[i].hemisphere
-		verts = vertices(montage.surface[which_hem], Bilateral(), Inclusive())
-		mesh!(
-			ax, montage.meshes[which_hem]..., color = colors[verts]; 
+			ax, montage.meshes[which_hem]..., color = values_to_plot[verts]; 
 			kwargs...
 		)
 	end
@@ -76,21 +41,25 @@ Makie.plot!(montage::Montage, args...; kwargs...) = mesh!(montage, args...; kwar
 
 ## helpers for plotting:
 
+import CorticalSurfaces: pad, default_with
+default_with(::Colorant) = NaN
+pad(x, montage::Montage) = pad(x, montage.surface)
+
+function align_values(values::AbstractVector, montage::Montage; kwargs...)
+	length(values) == size(montage.surface, Inclusive()) && 
+		return values
+	length(values) == size(montage.surface, Exclusive()) && 
+		return pad(values, montage; kwargs...)
+	return error(DimensionMismatch)
+end
+
 function colorize(
 		val::Real; 
-		colormap::Vector{<:Colorant}, 
-		colorrange,
-		lowclip = colormap[1], 
-		highclip = colormap[end]
+		colormap::Vector{<:Colorant}, colorrange, lowclip = colormap[1], highclip = colormap[end]
 	)
 	zmin, zmax = colorrange
-
-	if val <= zmin
-		return lowclip
-	elseif val >= zmax
-		return highclip
-	end
-
+	val <= zmin && return lowclip
+	val >= zmax && return highclip
 	scaled_val = (val - zmin) / (zmax - zmin) * (length(colormap) - 1)
 	color1 = colormap[floor(Int, scaled_val) + 1]
 	color2 = colormap[ceil(Int, scaled_val) + 1]
@@ -98,4 +67,28 @@ function colorize(
 	grad = cgrad([color1, color2])
 	return only(RGB{Float32}[grad[t]])
 end
+
+function compute_colors(values::AbstractVector; kwargs...)
+	return values
+end
+
+function compute_colors(values::AbstractVector{<:Integer}; kwargs...)
+	colormap = haskey(kwargs, :colormap) ? 
+		kwargs[:colormap] : 
+		distinguishable_colors(min(64, length(unique(values))))
+	if typeof(colormap) <: AbstractVector
+		colorrange = haskey(kwargs, :colorrange) ?
+			kwargs[:colorrange] :
+			(minimum(values), maximum(values))
+		colors = colorize.(values; colormap = colormap, colorrange = colorrange)
+	else # AbstractDict case
+		colors = [colormap[v] for v in values]
+	end
+	colors[medial_wall(montage.surface)] .= surf_color
+	return colors
+end
+
+
+
+
 
