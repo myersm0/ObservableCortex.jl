328d6a02edc1d726b4fc3f27e3117121f40a7ef1
Michael Myers <michael.myers@wustl.edu>
2025-08-26 10:20:43 -0500

    added animation demo for time series data

--- examples/basic_demo.jl
+++ examples/basic_demo.jl
@@ -12,31 +12,32 @@ include("surface_setup.jl")
 
 # ===== make a continuous-valued surface plot ==========================================
 
-fig = Figure(; size = (600, 400))
+fig = Figure(; size = (1200, 800))
 montage = Montage(panels = default_views, grid = fig.layout, surface = c)
 colors = collect(1.0:size(c, Exclusive()))
 colorrange = (minimum(colors), maximum(colors)) # to enforce consistency across panels
 plot!(montage, colors; colormap = coolhot, colorrange = colorrange)
+
+# you can then add arbitrary additional elements, like a colorbar:
 cbar = Colorbar(
-	fig, bbox = BBox(240, 360, 200, 180), 
+	fig, 
+	bbox = BBox(480, 720, 400, 360), 
 	limits = colorrange, 
 	colormap = coolhot, 
 	vertical = false, 
-	width = 120, 
-	height = 20, 
+	width = 240, 
+	height = 40, 
 	ticks = ([colorrange[1], colorrange[2]], ["1", "59412"]),
 	label = "Vertex",
 )
-save("demo1.png", fig)
 
 
 # ===== as above but this time providing the colors directly ===========================
 
-fig = Figure(; size = (800, 600))
+fig = Figure(; size = (1600, 1200))
 montage = Montage(panels = default_views, grid = fig.layout, surface = c)
 colors = [RGB(α, α, α) for α in range(0, 1; length = size(c, Exclusive()))]
-plot!(montage, colors; colormap = coolhot)
-save("demo2.png", fig)
+plot!(montage, colors)
 
 
 # ===== as above but this time with a custom arrangement of brain views ================
@@ -48,11 +49,11 @@ custom_views = PanelLayout(
 	]
 )
 
-fig = Figure(; size = (1200, 600))
+fig = Figure(; size = (1800, 900))
 montage = Montage(panels = custom_views, grid = fig.layout, surface = c)
 colors = [
 	[HSV(0, 0, v) for v in range(0, 1; length = size(c[L], Exclusive()))];
-	[HSV(0, 0, v) for v in range(1, 0; length = size(c[R], Exclusive()))];
+[HSV(0, 0, v) for v in range(1, 0; length = size(c[R], Exclusive()))];
 ]
 plot!(montage, colors; colormap = coolhot)
 colgap!(montage.grid, 2, -100)
@@ -61,8 +62,6 @@ colgap!(montage.grid, 3, -220)
 ax = axis(montage, (R, Medial))
 coord = coordinates(c[R])[:, 2099]
 meshscatter!(ax, coord'; color = :yellow, markersize = 4)
-
-save("demo3.png", fig)
 
 
 # ===== plot discrete-valued data such as a parcellation ===============================
@@ -75,10 +74,9 @@ parcel_file = joinpath(rootpath, "test_parcels.dtseries.nii")
 cifti_data = CIFTI.load(parcel_file)
 px = BilateralParcellation{Int}(c, cifti_data)
 
-fig = Figure(; size = (600, 400))
+fig = Figure(; size = (1200, 800))
 montage = Montage(panels = default_views, grid = fig.layout, surface = c)
 plot!(montage, px)
-save("demo4.png", fig)
 
 
 # ===== as above, but with a custom categorical colors =================================
@@ -89,14 +87,14 @@ save("demo4.png", fig)
 # make them all blue:
 colormap = Dict(k => RGB(0, 0.4, 1.0) for k in keys(px))
 
-fig = Figure(; size = (600, 400))
+fig = Figure(; size = (1200, 800))
 custom_view = PanelLayout(reshape([(L, Medial)], (1, 1)))
 montage = Montage(grid = fig.layout, surface = c, panels = custom_view)
 plot!(montage, px; colormap = colormap)
 
 # we can also use meshscatter!() to plot "borders" on top of the parcels like this:
 
-make_adjacency_list!(px.surface) # needed for finding borders below
+initialize_adjacencies!(px.surface) # needed for finding borders below
 
 ax = axes(montage, 1, 1)
 ks = collect(keys(px[L]))
@@ -104,7 +102,7 @@ for k in ks
 	p = px[L][k]
 	border_vertices = borders(p)
 	coords = coordinates(c[L])[:, border_vertices]
-	meshscatter!(ax, coords'; color = :yellow, markersize = 0.8)
+	meshscatter!(ax, coords'; color = :yellow, markersize = 0.65)
 end
 
 
@@ -129,10 +127,46 @@ colormap = Dict(
 	for (i, k) in enumerate(ks)
 )
 
-fig = Figure(; size = (600, 400))
+fig = Figure(; size = (1200, 800))
 montage = Montage(panels = default_views, grid = fig.layout, surface = c)
 plot!(montage, px; colormap = colormap)
 
-save("demo5.png", fig)
+
+
+# ===== animated example =====
+
+fig = Figure(; size = (1600, 1200))
+montage = Montage(panels = default_views, grid = fig.layout, surface = c)
+
+n_timepoints = 500
+n_vertices = size(c, Exclusive())
+
+# define some faux time series data for demonstration
+time_series = zeros(n_vertices, n_timepoints)
+Threads.@threads for t in 1:n_timepoints
+	phase = 2π * t / n_timepoints
+	for v in 1:n_vertices
+		x = (v / n_vertices) * 4π
+		time_series[v, t] = sin(x - phase)
+	end
+end
+
+current_frame = Observable(1)
+current_data = @lift time_series[:, $current_frame]
+
+plot!(montage, current_data; colormap = coolhot, colorrange = (-1, 1))
+
+control_grid = GridLayout(fig[3, 1:2])
+
+slider = Slider(control_grid[1, 1:3], range = 1:n_timepoints, startvalue = 1)
+connect!(current_frame, slider.value)
+Label(control_grid[1, 4], @lift("Frame: $($current_frame)/$n_timepoints"))
+
+
+
+
+
+
+
 
 
